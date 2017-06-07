fc3e2676f3fa200ae87a3483585bd0b3f6f8f3b7
sheawinkler <sheawinkler@gmail.com>
2017-06-07 11:28:58 -0500

    Add files via upload

--- Lab7/Lab7.sql
+++ Lab7/Lab7.sql
@@ -4,6 +4,7 @@ SELECT * FROM Band WHERE name LIKE 'The%';
 SELECT * FROM Song ORDER BY title;
 SELECT albumId FROM Album WHERE title LIKE 'Nevermind';
 SELECT COUNT(musicianId) FROM Musician;
-SELECT * FROM Album ORDER BY year LIMIT 1; /* Not exact to instructions */
+SELECT * FROM Album WHERE year = (SELECT MIN(year) FROM Album);
 SELECT SUM(trackLength) FROM AlbumSong WHERE albumId = 5; /* doesn't use album name */
 SELECT title, year, name FROM Album INNER JOIN Band ON Album.bandId = Band.bandId;
+SELECT * FROM AlbumSong WHERE albumId = (Select albumID FROM A);
