9a2da8e05e477ee525ab7f0c9be3c1aab8a03077
sheawinkler <sheawinkler@gmail.com>
2017-06-07 12:32:34 -0500

    Add files via upload
    Have everything completed through the 15th query, all former issues resolved. I'm not sure how to do number 16.

--- Lab7/Lab7.sql
+++ Lab7/Lab7.sql
@@ -5,6 +5,12 @@ SELECT * FROM Song ORDER BY title;
 SELECT albumId FROM Album WHERE title LIKE 'Nevermind';
 SELECT COUNT(musicianId) FROM Musician;
 SELECT * FROM Album WHERE year = (SELECT MIN(year) FROM Album);
-SELECT SUM(trackLength) FROM AlbumSong WHERE albumId = 5; /* doesn't use album name */
+SELECT SUM(trackLength) FROM AlbumSong WHERE albumId = (SELECT albumId FROM Album WHERE title = "Rain Dogs"); 
 SELECT title, year, name FROM Album INNER JOIN Band ON Album.bandId = Band.bandId;
-SELECT * FROM AlbumSong WHERE albumId = (Select albumID FROM A);
+SELECT Album.title AS Album, Song.title AS Song FROM Album JOIN AlbumSong ON Album.albumId = AlbumSong.albumId INNER JOIN Song ON AlbumSong.songId = Song.songId JOIN Band ON Band.bandId = Album.bandID WHERE Band.name = "Nirvana";
+SELECT Band.name AS Band , Album.title AS Album FROM Band LEFT JOIN Album ON Band.bandId = Album.bandId;
+SELECT Band.name AS Band , COUNT(Album.title) AS Albums FROM Band LEFT JOIN Album ON Band.bandId = Album.bandId GROUP BY Band.name;
+SELECT Album.title AS Album , COUNT(AlbumSong.albumSongId) AS Tracks FROM Album JOIN AlbumSong ON Album.albumId = AlbumSong.albumId GROUP BY Album.title HAVING Tracks > 11;
+SELECT Musician.firstName AS FirstName, Musician.lastName as LastName FROM Musician JOIN BandMember ON Musician.musicianId = BandMember.musicianId;
+SELECT * FROM Musician WHERE 'Musician.musicianId' NOT IN (SELECT 'BandMember.musicianId' FROM BandMember);
+
